e48a5c17002d2c61b845a0cc4f8f40007cb4a1f1
NatChantra <natchantra2005@email@gmail.com>
2026-07-30 09:58:09 +0700

    fix HrAdminPage import

--- src/pages/HrAdminPage.jsx
+++ src/pages/HrAdminPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { api } from "../services/api";
+import { employeeApi, leaveApi, deptApi } from "../services/api";
 import styles from "./HrAdminPage.module.css";
 
 export default function HrAdminPage() {
