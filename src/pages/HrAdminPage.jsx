9f0a02b6446f3ede745cfea020a37e2a1565f454
NatChantra <natchantra2005@email@gmail.com>
2026-07-30 09:25:10 +0700

    Add basic HrAdminPage

---
+++ src/pages/HrAdminPage.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { api } from "../services/api";
+import styles from "./HrAdminPage.module.css";
+
+export default function HrAdminPage() {
+  return <div><h2>HR Administration</h2></div>;
+}
